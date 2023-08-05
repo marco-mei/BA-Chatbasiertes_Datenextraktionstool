ac5cce6e7da3780f7ab5c343803aaa025fa94855
Marco Meinerzhagen <m.meinerzhagen@thinkbic.de>
2023-08-05 18:47:15 +0200

    Added step1 method

--- evaluate.py
+++ evaluate.py
@@ -6,12 +6,20 @@ with open("questions.json", "r", encoding="utf-8") as f:
     questions = json.load(f)
 
 
+def startModel(path, question):
+    result = startLanguageModel(path, question["question"])
+    print(f"Frage {question['id']}: {question['question']}:")
+    print(f"{result}\n")
+
+
 def evaluateStep1():
-    path = "../BA-Chatbasiertes_Datenextraktionstool/data_ifc_models/Beispielbüro.ifc"
+    path = "../BA-Chatbasiertes_Datenextraktionstool/data_ifc_models/Testmodell.ifc"
+
+    # startModel(path, questions[7])
+
     for question in questions:
-        result = startLanguageModel(path, question["question"])
-        print(result)
-        time.sleep(20)
+        startModel(path, question)
+        time.sleep(100)
 
 
 if __name__ == '__main__':
--- step1/startLanguageModel.py
+++ step1/startLanguageModel.py
@@ -1,60 +1,70 @@
+import os
+import tiktoken
 from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
-from langchain.chat_models import AzureChatOpenAI, ChatOpenAI
-from langchain.document_loaders import CSVLoader, DirectoryLoader, TextLoader
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.indexes import VectorstoreIndexCreator
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.llms import OpenAI, OpenAIChat
-import os
-
 from langchain.vectorstores import Chroma
 
 
+def num_tokens_from_string(path) -> int:
+    """Returns the number of tokens in a text string."""
+    string: str = open(path, "r").read()
+    encoding = tiktoken.get_encoding("cl100k_base")
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
 def startLanguageModel(path, query):
-    os.environ["OPENAI_API_KEY"] = "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"
-    print(query)
+    os.environ["OPENAI_API_KEY"] = "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"  # personal key
 
     # Load the document
     loader = TextLoader(path)
     ifc_model = loader.load()
 
     # Split the document into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=39, chunk_overlap=0, separators=[" ", ",", "\n", ":"])
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 
     texts = text_splitter.split_documents(ifc_model)
     embeddings = OpenAIEmbeddings()
-    db = Chroma.from_documents(texts, embeddings, persist_directory="./data/data-01.db")
 
-    # Create an index using the loaded documents
-    docsearch = VectorstoreIndexCreator().from_loaders([loader])
-    print("test")
+    # Create a vectorstore from the documents
+    docsearch = Chroma.from_documents(texts, embeddings)
 
     # Prompt template
     prompt_template = """Given the following ifc file that contains information about a building information model with filetype
                 ifc and a question, create a final answer.
 
                 Respond in German.
-
-                {context}
+                
+                Model name: {context}
 
                 QUESTION: {question}
                 =========
                 FINAL ANSWER IN German:"""
 
     # Create a prompt using the template
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    prompt = PromptTemplate(template=prompt_template, input_variables=["question", "context"])
+
+    # Create context
+    context = path.split("/")[-1].split(".")[0]
 
     # Prompt kwargs
     chain_type_kwargs = {"prompt": prompt}
 
-    # Define document name as context
-    context = "IFC-Modell: " + path.split("/")[-1].split(".")[0]
-
     # Create a question-answering chain using the index
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo')
-    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), chain_type_kwargs=chain_type_kwargs)
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo-16k')
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
 
     # Pass a query to the chain
-    response = chain({"query": query, "context": context})
+    response = chain({"query": query})
     return response['result']
+
+
+if __name__ == '__main__':
+    path = "../data_ifc_models/Testmodell.ifc"
+    query = "Welche IFC Version hat das Modell?"
+    print("Mit welcher CAD Anwendung wurde das Modell erstellt?")
+    print(startLanguageModel(path, "Mit welcher CAD Anwendung wurde das Modell erstellt?"))
